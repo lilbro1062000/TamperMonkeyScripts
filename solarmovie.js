662fa75ed12043149e60bde1d0beb34e026471b8
Abdoulaye Camara <lilbro1062000@gmail.com>
2015-05-04 20:18:25 -0400

    Update solarmovie.js
    Added new script

--- solarmovie.js
+++ solarmovie.js
@@ -1,1 +1,21 @@
- 
+ // ==UserScript==
+// @name         My Fancy New Userscript
+// @namespace    http://your.homepage/
+// @version      0.1
+// @description  enter something useful
+// @author       You
+// @match        http://www.solarmovie.is/link/play/*
+// @grant        none
+// ==/UserScript==
+
+var header =document.getElementById('frameWrapper');
+header.style.display='none';
+
+var center = document.getElementsByClassName('thirdPartyEmbContainer')[0];
+
+center.style.padding='0px';
+center.style.marginLeft='-300px';
+
+var foot = document.getElementById('footer');
+
+foot.style.display = 'none';
